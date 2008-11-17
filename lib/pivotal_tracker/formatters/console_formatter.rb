a88ead0a1f27c638bea0fabfae7ee8bbb3a53f5d
Corey Innis <corey@coolerator.net>
2008-11-17 14:33:04 -0800

    more clean up of non-adapter stuff

--- lib/pivotal_tracker/formatters/console_formatter.rb
+++
@@ -1,7 +0,0 @@
-class PivotalTracker::Formatters::ConsoleFormatter
-  def format(stories, options)
-    stories.each do |story|
-      puts "Story: #{story.name} (#{story.estimate})"
-    end
-  end
-end
--- spec/pivotal_tracker_spec.rb
+++ spec/pivotal_tracker_spec.rb
@@ -1,38 +1,5 @@
-require File.join(File.dirname(__FILE__), "spec_helper")
+dir = File.join(File.dirname(__FILE__), 'pivotal_tracker')
 
-require "pivotal_tracker"
-
-describe PivotalTracker do
-  describe "default_options" do
-    it "should default to the console formatter" do
-      PivotalTracker.default_options[:formatter].should == "console"
-    end
-
-    it "should default file_name to stories.pdf" do
-      PivotalTracker.default_options[:file_name].should == "stories.pdf"
-    end
-  end
-
-  describe "validate_options" do
-    before :each do
-      @valid_options = {
-        :project_id => "111",
-        :token      => "AAasdADasdASD"
-      }
-    end
-
-    it "should return true if all required options are present" do
-      PivotalTracker.validate_options(@valid_options).should == true
-    end
-
-    it "should return false if project_id is missing" do
-      @valid_options[:project_id] = nil
-      PivotalTracker.validate_options(@valid_options).should == false
-    end
-
-    it "should return false if token is missing" do
-      @valid_options[:token] = nil
-      PivotalTracker.validate_options(@valid_options).should == false
-    end
-  end
+Dir["#{dir}/**/*.rb"].each do |file|
+  require file
 end
